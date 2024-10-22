3b0750edd8b2f4084d1e107c712426b2f7a52c4c
rate1 <lislislis1986@gmail.com>
2024-10-22 17:19:38 +0800

    Написание строк документации для всех функций

--- content_parser/youtube_parser.py
+++ content_parser/youtube_parser.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 
@@ -9,8 +9,19 @@ from youtube_transcript_api.formatters import TextFormatter
 def youtube_video_id_parser(YOUTUBE_API_KEY: str,
                             query: str,
                             max_results: int) -> list[str]:
-    """Функция парсит выдачу Youtube по запросу
-    query и возвращает список ID видео из первых max_results результатов"""
+    """
+    Парсит выдачу YouTube по запросу.
+    Парсит выдачу YouTube по запросу query и возвращает список результатов в
+    количестве max_results штук.
+    Args:
+        YOUTUBE_API_KEY(str): API ключ YouTube.
+        query(str): Поисковый запрос.
+        max_results(int): Максимальное количество получаемых результатов.
+    Returns:
+        list[str]: Список id видео из первых результатов поиска в количестве
+                    max_results штук.
+    Raises:
+    """
     url_youtube_data_api = (f"https://www.googleapis.com/youtube/v3/search?"
                             f"part=snippet&"
                             f"maxResults={max_results}&"
@@ -35,20 +46,19 @@ def youtube_video_id_parser(YOUTUBE_API_KEY: str,
 
 def youtube_subtitles_parser(video_id: str, language_code="en") -> str:
     """
-    Парсит субтитры c youtube на указанном языке.
-    :param video_id: ID видео на YouTube.
-    :param language_code: Язык субтитров, по умолчанию "en".
-    :return: Субтитры на указанном языке или None, если их нет.
+    Парсит субтитры c YouTube на указанном языке.
+    Args:
+        video_id(str): ID видео на YouTube.
+        language_code(str): Язык субтитров, по умолчанию "en".
+    Returns:
+        str: Субтитры на указанном языке или None, если их нет.
     """
     try:
-        # Получаем список доступных субтитров для видео
         transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-        # Пробуем получить субтитры на указанном языке
         if language_code in transcript_list._manually_created_transcripts:
             transcript = transcript_list.find_manually_created_transcript([language_code])
         else:
             transcript = transcript_list.find_generated_transcript([language_code])
-        # Форматируем субтитры как текст
         formatter = TextFormatter()
         subtitles_text = formatter.format_transcript(transcript.fetch())
         return subtitles_text
@@ -57,16 +67,32 @@ def youtube_subtitles_parser(video_id: str, language_code="en") -> str:
         return None
 
 
-def split_text(text, chunk_size):
-    """Разделяет текст на части указанного размера"""
+def split_text(text: str, chunk_size: int) -> list[str]:
+    """
+    Разделяет текст на части указанного размера.
+    Args:
+        text(str): Текст для разбивания на части.
+        chunk_size(int): Размер получаемых частей.
+    Returns:
+        list[str]: Список, содержащий части поделенного текста.
+    Raises:
+    """
     return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
 
 
 def text_translator_mymemory(text: str, src='en', dest='ru') -> str:
-    """Переводит полученный текст с языка src, на язык dest, используя 
-    бесплатный API MyMemory"""
-
-    # URL для MyMemory API
+    """
+    Переводит текст.
+    Переводит полученный текст с языка src, на язык dest, используя
+    бесплатный API MyMemory.
+    Args:
+        text(str): Текст для перевода.
+        src(str): Язык оригинального текста для перевода.
+        dest(str): Язык, на который необходимо произвести перевод.
+    Returns:
+        str: Переведенный текст.
+    Raises:
+    """
     url = "https://api.mymemory.translated.net/get"
     translated_text = []
     chunks = split_text(text, 500)
@@ -78,13 +104,11 @@ def text_translator_mymemory(text: str, src='en', dest='ru') -> str:
         response = requests.get(url, params=params)
         if response.status_code == 200:
             json_response = response.json()
-            translated_chunk= json_response['responseData']['translatedText']
+            translated_chunk = json_response['responseData']['translatedText']
             translated_text.append(translated_chunk)
         else:
             print(f"Ошибка перевода части текста: {response.status_code}")
             break
-    return translated_text
-    
     print("Переведённые субтитры:", translated_text)
     return ' '.join(translated_text)
 
@@ -92,29 +116,19 @@ def text_translator_mymemory(text: str, src='en', dest='ru') -> str:
 def main():
     load_dotenv()
     YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')
-    query = 'top 5 anime in the world'
+    query = input("Введите запрос для поиска видео на ютуб: ")
     max_results = 5
-    #res = youtube_video_id_parser(YOUTUBE_API_KEY, query, max_results)
-    #print(res)
+    res = youtube_video_id_parser(YOUTUBE_API_KEY, query, max_results)
+    print(res)
     subtitles = ""
-    for i in range(5):
-        #    subtitles = youtube_subtitles_parser(res[i])
-        if subtitles:
-            print("===================================================")
-            res = text_translator_mymemory(subtitles)
-            print(res)
-            break
-        print("===================================================")
-    test2 = youtube_subtitles_parser('Yd95LBhuSOk', 'ru')
-    if test2:
-        print("=================оригинал ютуб====================")
-        print(test2)
-        print("=================оригинал ютуб====================")
+    subtitles = youtube_subtitles_parser(res[0], 'ru')
+    if subtitles:
+        print("Оригинальные субтитры YouTube на русском языке:")
+        print(subtitles)
     else:
-        print("*******************перевод***************")
-        test2 = youtube_subtitles_parser('Yd95LBhuSOk', 'en')
-        print(text_translator_mymemory(test2))
-        print("*******************перевод***************")
+        print("Переведенные субтитры YouTube на русском языке:")
+        subtitles = youtube_subtitles_parser(res[0], 'en')
+        print(text_translator_mymemory(subtitles))
 
 
 if __name__ == '__main__':
--- rewrite/chatgpt_rewrite.py
+++ rewrite/chatgpt_rewrite.py
@@ -3,30 +3,45 @@ import os
 from openai import OpenAI
 
 
-def gpt_rewrite(text):
-
-    print(os.environ.get("OPENAI_API_KEY"))
-
-    client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
-    )
-    # Вызов GPT для рерайта текста
+def gpt_rewrite(text: str, temperature=0.7, max_tokens=1500) -> str:
+    """
+    Делает рерайт текста с помощью chatgpt.
+    Args:
+        text(str): Текст для рерайта.
+        temperature(float): Значение в диапазоне [0;1], чем больше число, тем
+        более креативным будет ответ. По умолчанию значение 0.7.
+        max_tokens(int): Максимальное количество токенов chatgpt, по умолчанию
+        1500 штук.
+    Returns:
+        str: Измененный с помощью рерайта текст.
+    Raises:
+    """
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"),)
     response = client.chat.completions.create(
         model="gpt-4o-mini",  # Или другой доступный движок GPT
         messages=[
             {
                 "role": "system",
-                "content": ("Ты David Ogilvy,занимаешься качественным "
-                            "копирайтом текста. Профессионал своего дела.")
+                "content": (
+                    "Ты David Ogilvy,занимаешься качественным "
+                    "копирайтом текста. Профессионал своего дела.")
              },
-             {
+            {
                  "role": "user",
-                 "content": f"Переформулируй указанный после двоеточия текст и адаптируй его так, чтобы он идеально подходил для публикации в виде статьи на Яндекс.Дзен. Очеловечь этот текст и убери из него предложения лишенные смысла, оживи и создай сенсацию, чтобы текст вызывал желание дочитать статью до самого конца:\n\n{text}"
+                 "content": (
+                    f"Переформулируй указанный после двоеточия текст "
+                    f"и адаптируй его так, чтобы он идеально подходил "
+                    f"для публикации в виде статьи на Яндекс.Дзен. "
+                    f"Очеловечь этот текст и убери из него "
+                    f"предложения лишенные смысла, оживи и создай "
+                    f"сенсацию, чтобы текст вызывал желание дочитать "
+                    f"статью до самого конца. Из текста необходимо "
+                    f"убрать прямую речь от имени рассказчика: "
+                    f"\n\n{text}")
              }
         ],
-        max_tokens=1000,
-        temperature=0.7
+        max_tokens=max_tokens,
+        temperature=temperature
     )
 
     return response.choices[0].message
